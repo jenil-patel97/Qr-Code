cb983a913cc6b62c7f40a636b57cb6a965f9df8e
Jenil Patel <98316039+jenil-patel97@users.noreply.github.com>
2022-10-30 11:00:23 +0530

    feat : App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React from "react";
 
 function App() {
   return (
-    <div className="text-center text-3xl mt-2 font-semibold font-Viaoda ">
-      Hello World !
+    <div className="bg-gradient-to-r from-cyan-500 to-blue-500 h-screen pt-36 px-2">
+      <div className="container mx-auto max-w-4xl bg-white rounded-md shadow">
+        <div className="md:grid md:grid-cols-3">
+          <h1>Input</h1>
+          <h1>Qr Code</h1>
+        </div>
+      </div>
     </div>
   );
 }
