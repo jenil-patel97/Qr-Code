bb6c489d09cf19fb237edc690af35db7ec66f067
Jenil Patel <98316039+jenil-patel97@users.noreply.github.com>
2022-10-30 12:44:48 +0530

    fix: InputColor

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import InputForm from "./components/InputForm";
 
 function App() {
   return (
-    <div className="bg-gradient-to-r from-cyan-500 to-blue-500 h-screen pt-36 px-2">
+    <div className="bg-gradient-to-r from-cyan-500 to-blue-500 h-screen pt-[85px] px-2">
       <div className="container mx-auto max-w-4xl bg-white rounded-md shadow">
         <div className="md:grid md:grid-cols-3">
           <InputForm />
--- src/components/InputColor.tsx
+++ src/components/InputColor.tsx
@@ -1,8 +1,12 @@
-import { useState } from "react";
+import { SetStateAction, useState } from "react";
+import ColorPicker from "react-pick-color";
 
 const InputColor = () => {
   const [color, setColor] = useState("#054080");
   const [displayColorPicker, setDisplayColorPicker] = useState(false);
+
+  const handleChange = (color: { hex: SetStateAction<string> }) =>
+    setColor(color.hex);
 
   return (
     <div>
@@ -11,11 +15,15 @@ const InputColor = () => {
         <div
           onClick={() => setDisplayColorPicker(!displayColorPicker)}
           style={{ background: color }}
-          className="w-10 h-8 cursor-pointer border-[3px] border-zinc-300"
+          className="w-11 h-9 mt-2 cursor-pointer border-[3px] border-stone-400"
         ></div>
         <span className="font-medium">{color}</span>
       </div>
-      {displayColorPicker && "ShowcolorPicker"}
+      {displayColorPicker && (
+        <div className="absolute mt-2">
+          <ColorPicker color={color} onChange={handleChange} />
+        </div>
+      )}
     </div>
   );
 };
