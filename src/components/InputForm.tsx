4a06b5551a4e2ade2cff14a78bdf59bff15f025b
Jenil Patel <98316039+jenil-patel97@users.noreply.github.com>
2022-10-31 10:24:20 +0530

    feat: button

--- src/components/InputForm.tsx
+++ src/components/InputForm.tsx
@@ -6,6 +6,9 @@ const InputForm = () => {
     <div className="col-span-2 p-6 grid gap-4">
       <InputField />
       <InputColor />
+      <button className="bg-blue-500 max-w-xs ml-auto px-4 py-2 text-white rounded-md mt-4 hover:bg-blue-600">
+        Generate QR-Code
+      </button>
     </div>
   );
 };
