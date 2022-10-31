5397bed5bc06cc31b25007a80177126bde27e712
Jenil Patel <98316039+jenil-patel97@users.noreply.github.com>
2022-10-31 12:27:15 +0530

    fix : App

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,8 @@
-import InputForm from "./components/InputForm";
-import QrCode from "./components/QrCode";
 
 function App() {
   return (
-    <div className="bg-gradient-to-r from-cyan-500 to-blue-500 h-screen pt-[85px] px-2">
-      <div className="container mx-auto max-w-4xl bg-white rounded-md shadow">
-        <div className="md:grid md:grid-cols-3">
-          <InputForm />
-          <QrCode/>
-        </div>
-      </div>
+    <div className="">
+      
     </div>
   );
 }
